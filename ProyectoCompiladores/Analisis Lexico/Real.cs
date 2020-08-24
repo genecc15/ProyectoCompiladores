a608b881e7b10162cf7598a583871ee3f73ffd57
genecc15 <36336252+genecc15@users.noreply.github.com>
2020-08-23 20:56:31 -0600

    Numeros reales
    Aquellos que tienen punto

---
+++ ProyectoCompiladores/Analisis Lexico/Real.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProyectoCompiladores.Tokens;
+
+namespace ProyectoCompiladores.Analisis_Lexico
+{
+    public class Real : Token
+    {
+        private float valorField;
+        public float Valor { get { return valorField; } }
+        public Real(float v) : base(((double)v).ToString("0.0#####"), TokenType.FLOTANTE)
+        {
+            valorField = v;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
