ceab78e88dad76c3f2ca24fdf04355ea258c2602
genecc15 <36336252+genecc15@users.noreply.github.com>
2020-08-23 18:38:37 -0600

    Diseño básico

--- ProyectoCompiladores/Form1.Designer.cs
+++ ProyectoCompiladores/Form1.Designer.cs
@@ -29,12 +29,64 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnAnalisisLexico = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 105);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(350, 350);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnAnalisisLexico
+            // 
+            this.btnAnalisisLexico.Location = new System.Drawing.Point(418, 301);
+            this.btnAnalisisLexico.Name = "btnAnalisisLexico";
+            this.btnAnalisisLexico.Size = new System.Drawing.Size(155, 73);
+            this.btnAnalisisLexico.TabIndex = 2;
+            this.btnAnalisisLexico.Text = "Analizar";
+            this.btnAnalisisLexico.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(613, 105);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(350, 350);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(946, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnAnalisisLexico);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnAnalisisLexico;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
